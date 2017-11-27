811b47546a5abe72efd04d992e678433ea8f6245
TomorrowStars <chenhongsheng2012@163.com>
2017-11-27 14:47:09 +0800

    20171127_chen_http通讯实例1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   imports: [
-  BrowserModule],
-  declarations: [AppComponent],
+    BrowserModule,
+    HttpModule,
+    FormsModule  
+  ],
+  declarations: [AppComponent, ProductComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  datasource: Observable<any>;
+  products: Array<any> = [];
+
+  constructor(private http: Http) {
+    this.datasource = http.get('api/products')
+    .map((res) => res.json());
+   }
+
+  ngOnInit() {
+    this.datasource.subscribe((data) => this.products = data);
+  }
+
+}
